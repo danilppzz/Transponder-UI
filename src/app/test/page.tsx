d92778ef9e749f5a550d4ac53cc30808135e405f
d4niz <josedanielmorebolopez@gmail.com>
2024-08-26 01:31:10 +0200

    Update

---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="bg-black text-white h-screen flex flex-col gap-8 items-center justify-center">
+      <div className="backgroundpage border border-white/20 bg-black h-[350px] w-[350px] rounded-2xl relative">
+        <div className="absolute bottom-0 bg-opacity-50 bg-black backdrop-blur-sm w-full h-2/6 rounded-b-2xl">
+          <div className="h-full w-full px-4 py-2">
+            
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
